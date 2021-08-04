5cfdd542e878c32c0d29873116c5cbf0ce0dc6fc
shamdzh <shdzhanhotov@mail.ru>
2021-08-04 04:53:54 +0300

    Load project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+function App() {
+  return (
+    
+        <div className="container pt-4">
+            <h1>Hello, World!</h1>
+        </div>
+      
+  );
+}
+
+export default App;
