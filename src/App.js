a890c3ba7f3bf1b792822f124aced24389ef167f
shamdzh <shdzhanhotov@mail.ru>
2021-08-29 23:42:47 +0300

    анимация перехода между страницами

--- src/App.js
+++ src/App.js
@@ -2,23 +2,37 @@ import React from "react";
 import { Navbar } from "./components/Navbar";
 import { FirebaseState } from "./context/firebase/firebaseState";
 import { Main } from "./pages/Main";
-import { BrowserRouter, Switch, Route } from "react-router-dom"
+import { BrowserRouter, Switch, Route, useLocation } from "react-router-dom";
 import { Note } from "./pages/Note";
+import { useTransition, animated } from "react-spring";
 
 function App() {
+  const location = useLocation();
+  console.log(location);
+  const transitions = useTransition(location, { 
+    from: { opacity: 0 },
+    enter: { opacity: 1 },
+    leave: { opacity: 0, display:'none' },
+    config: { duration: 700 }
+   
+  });
+
   return (
     <FirebaseState>
-     <BrowserRouter>
-      <Navbar /> 
-      <div className="container pt-5">
-        <Switch>
-          <Route exact path="/" component={Main}/>
-          <Route exact path="/note" component={Note}/>
-        </Switch>
+      <Navbar />
+      <div className="container pt-5" style={{position:'relative', height: "90vh"}}>
+        {transitions((props, item) => (
+          <animated.div style={props}>
+            <div style={{position: "absolute", width:"100%"}}>
+            <Switch location={item}>
+              <Route exact path="/" component={Main} />
+              <Route exact path="/note" component={Note} />
+            </Switch>
+            </div>
+          </animated.div>
+        ))}
       </div>
-      </BrowserRouter>
     </FirebaseState>
-    
   );
 }
 
