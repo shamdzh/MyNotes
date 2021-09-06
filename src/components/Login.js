7f14ee1f55c7b1c7f23a0e09d4b6751413017950
shamdzh <shdzhanhotov@mail.ru>
2021-09-06 07:57:14 +0300

    Авторизация

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,22 +1,38 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { GoogleContext } from "..";
 import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { useAuthState } from "react-firebase-hooks/auth";
 
 export const Login = () => {
   const { auth } = useContext(GoogleContext);
-  const [userName, setName] = useState('');
-  
+  const [user] = useAuthState(auth);
+  const [userName, setName] = useState("");
+
+  useEffect(() => { 
+    console.log(user)
+
+    if (user) {
+      console.log("Вы авторизованы");
+      console.log(user)
+      setName(user.displayName);
+    } else {
+      console.log("Вы не авторизованы");
+      console.log(user)
+      setName("Вы не авторизованы");
+    } 
+  }, []);
 
   const login = () => {
     const provider = new GoogleAuthProvider();
 
-    signInWithPopup(auth, provider).then((result) => {
-      console.log(result)
-      setName(result.user.displayName)
-
-    }).catch((error) => {
-      console.log(error)
-    })
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        console.log(result);
+        setName(result.user.displayName);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
 
     console.log(provider);
   };
@@ -26,6 +42,17 @@ export const Login = () => {
       <button class="authBtn btn btn-primary" type="submit" onClick={login}>
         Войти
       </button>
+
+      <button
+        class="authBtn btn btn-primary"
+        type="submit"
+        onClick={() => {
+          console.log(auth.signOut());
+        }}
+      >
+        Выйти
+      </button>
+     
       <div>Вы вошли как: {userName}</div>
     </div>
   );
